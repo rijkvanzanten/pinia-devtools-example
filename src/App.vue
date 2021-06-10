b7600280a8e10333d546ddc3bafb8e830587263b
rijkvanzanten <rijkvanzanten@me.com>
2021-06-10 14:42:07 -0400

    Setup empty boilerplate

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<p>
+		Counter: {{ count }}
+		<button @click="increment">+</button>
+	</p>
+</template>
+
+<script>
+import { defineComponent, toRefs } from "vue";
+import { useCounterStore } from "./stores/counter";
+
+export default defineComponent({
+	setup() {
+		const counter = useCounterStore();
+
+		const { count } = toRefs(counter);
+
+		return { count, increment };
+
+		function increment() {
+			counter.count++;
+		}
+	},
+});
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
